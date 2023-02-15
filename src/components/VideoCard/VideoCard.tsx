445e057789923efbdc60eeadda3447b032f2aeb0
Darkerros <Darkerros@yandex.ru>
2023-02-16 01:29:13 +0300

    feat: complete video card

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -1,12 +1,14 @@
-import React, {FC, useCallback} from 'react';
+import React, {FC, useCallback, useState} from 'react';
 import styles from './VideoCard.module.css'
-import {VideoResourceType} from "../../api/types/types";
+import {ChannelResourceType, VideoResourceType} from "../../api/types/types";
+import {Link} from "react-router-dom";
 
 interface IProps {
     videoInfo:  VideoResourceType
 }
 
 const VideoCard:FC<IProps> = ({videoInfo}) => {
+    const [userInfo,setUserInfo] = useState<ChannelResourceType>()
     const formateViewCount = useCallback((viewCount: number) => {
         if (viewCount > 10000000) {
             return `${Math.round(viewCount/1000000)} млн просмотров`
@@ -23,13 +25,21 @@ const VideoCard:FC<IProps> = ({videoInfo}) => {
 
     return (
         <div className={styles.container}>
-            <img className={styles.image} src={videoInfo.snippet.thumbnails.medium?.url} alt=""/>
+            <Link to={`/video/${videoInfo.id}`} className={styles.link}>
+                <img className={styles.image} src={videoInfo.snippet.thumbnails.medium?.url} alt=""/>
+            </Link>
             <div className={styles.baseVideoDetailsContainer}>
-                {false ? <img className={styles.userImage} src="" alt=""/> : <span className={styles.userImage}/>}
-                <h2 className={styles.title}>{videoInfo.snippet.title}</h2>
+                <Link to={`/channel/${videoInfo.snippet.channelId}`} className={styles.link}>
+                    <img referrerPolicy={"no-referrer"} className={styles.userImage} src={userInfo?.snippet.thumbnails.medium?.url} alt=""/>
+                </Link>
+                <Link to={`/video/${videoInfo.id}`} className={styles.link}>
+                    <h2 className={styles.title}>{videoInfo.snippet.title}</h2>
+                </Link>
             </div>
             <div className={styles.additionalVideoDetailsContainer}>
-                <p className={styles.userName}>{videoInfo.snippet.channelTitle}</p>
+                <Link to={`/channel/${videoInfo.snippet.channelId}`} className={`${styles.userLink} ${styles.link}`}>
+                    <p className={styles.userName}>{videoInfo.snippet.channelTitle}</p>
+                </Link>
                 <p className={styles.viewCount}>{formateViewCount(videoInfo.statistics.viewCount)}</p>
             </div>
         </div>
