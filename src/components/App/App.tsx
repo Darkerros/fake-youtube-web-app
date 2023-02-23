166da58c5d82862fa6eacffc685a84394240bc35
Darkerros <Darkerros@yandex.ru>
2023-02-23 17:49:51 +0300

    feat: add new routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,16 +3,19 @@ import {Route, Routes} from "react-router";
 import Layout from "../Layout/Layout";
 import MainPage from "../../pages/MainPage/MainPage";
 import VideoPage from "../../pages/VideoPage/VideoPage";
+import ChannelPage from "../../pages/ChannelPage/ChannelPage";
+import CategoryPage from "../../pages/CategoryPage/CategoryPage";
 
 function App() {
 
     return (
-        <div className={`${styles.app} ${styles.app_theme_light}`}>
+        <div className={`${styles.app} ${styles.app_theme_dark}`}>
             <Routes>
                 <Route path={"/"} element={<Layout/>}>
                     <Route index element={<MainPage/>}/>
-                    <Route path={"channel/:id"} element={<MainPage/>}/>
+                    <Route path={"channel/:id"} element={<ChannelPage/>}/>
                     <Route path={"video/:id"} element={<VideoPage/>}/>
+                    <Route path={"category/:type"} element={<CategoryPage/>}/>
                 </Route>
             </Routes>
         </div>
