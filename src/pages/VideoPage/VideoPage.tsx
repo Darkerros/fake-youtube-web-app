c951ccbac5b46b9980c28fc71bcd27b8b02c6eb1
Darkerros <Darkerros@yandex.ru>
2023-02-23 17:48:29 +0300

    feat: complete Video page with thunks and new components

--- src/pages/VideoPage/VideoPage.tsx
+++ src/pages/VideoPage/VideoPage.tsx
@@ -1,43 +1,50 @@
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import styles from './VideoPage.module.css'
 import {useParams} from "react-router";
-import {api} from "../../api/api";
-import {ChannelResourceType, VideoResourceType} from "../../api/types/types";
 import ReactPlayer from "react-player";
 import VideoDescription from "../../components/VideoDescription/VideoDescription";
 import VideoContentDetails from "../../components/VideoContentDetails/VideoContentDetails";
+import VideoRecommendCard from "../../components/VideoRecommendCard/VideoRecommendCard";
+import VideoCommentsSection from "../../components/VideoCommentsSection/VideoCommentsSection";
+import {useAppDispatch} from "../../hooks/redux/useAppDispatch";
+import {getChannelInfoThunk} from "../../services/thunk/getChannelInfoThunk";
+import {useAppSelector} from "../../hooks/redux/useAppSelector";
+import {getVideoByIdThunk} from "../../services/thunk/getVideoByIdThunk";
+import {getRecomendVideoByCategoryIdThunk} from "../../services/thunk/getRecomendVideoByCategoryIdThunk";
 
 const VideoPage = () => {
+    const dispatch = useAppDispatch()
     const {id} = useParams<{ id: string }>()
-    const [videoInfo, setVideoInfo] = useState<VideoResourceType | null>(null)
-    const [channelInfo, setchannelInfo] = useState<ChannelResourceType | null>(null)
+    const channelInfo = useAppSelector(state => state.channel.data)
+    const videoInfo = useAppSelector(state => state.video.data)
+    const recommendVideoList = useAppSelector(state => state.recomendVideos.items)
 
     useEffect(() => {
-        if (id && !videoInfo) {
-            api.getVideoById(id).then(data => setVideoInfo(data.items[0]))
+        if ((id && !videoInfo) || (id && videoInfo?.id !== id)) {
+            dispatch(getVideoByIdThunk(id))
         }
-    }, [])
+    }, [id])
 
     useEffect(() => {
-        if (videoInfo && !channelInfo) {
-            api.channel(videoInfo.snippet.channelId).then(data => setchannelInfo(data.items[0]))
+        if ((videoInfo && !channelInfo) || (videoInfo && channelInfo?.id !== videoInfo.snippet.channelId)) {
+            dispatch(getChannelInfoThunk(videoInfo.snippet.channelId))
+            dispatch(getRecomendVideoByCategoryIdThunk(videoInfo.snippet.categoryId))
         }
-    },[videoInfo])
-
-    console.log(videoInfo,channelInfo)
+    }, [videoInfo])
 
     return (
-        <main className={styles.container}>
+        <section className={styles.container}>
             <div className={styles.videoContentContainer}>
                 <ReactPlayer url={`https://www.youtube.com/watch?v=${videoInfo?.id}`} controls={true} width={"100%"} height={"600px"} style={{borderRadius: "10px", overflow: "hidden"}}/>
                 <h2 className={styles.title}>{videoInfo?.snippet.title}</h2>
                 {videoInfo && channelInfo && <VideoContentDetails videoInfo={videoInfo} channelInfo={channelInfo}/>}
-                {videoInfo && <VideoDescription description={videoInfo.snippet.description}/>}
+                {videoInfo && videoInfo.snippet.description && <VideoDescription description={videoInfo.snippet.description}/>}
+                {id && <VideoCommentsSection videoId={id}/>}
             </div>
             <div className={styles.recommendationContainer}>
-
+                {recommendVideoList.map(videoInfo => <VideoRecommendCard videoInfo={videoInfo} key={videoInfo.id}/>)}
             </div>
-        </main>
+        </section>
     );
 };
 
