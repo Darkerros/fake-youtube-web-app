7bef3e4a8d297d5a5e675cb65f9ead6c585426cd
Darkerros <Darkerros@yandex.ru>
2023-02-23 17:34:31 +0300

    feat: create video reccomend card

---
+++ src/components/VideoRecommendCard/VideoRecommendCard.tsx
@@ -0,0 +1,33 @@
+import React, {FC, memo} from 'react';
+import styles from './VideoRecommendCard.module.css'
+import useViewsFormater from "../../hooks/useViewsFormater";
+import ViewIcon from "../../ui/icons/ViewIcon/ViewIcon";
+import {Link} from "react-router-dom";
+import {VideoResourceType} from "../../utils/types/api/resources/VideoResourceType";
+
+interface IProps {
+    videoInfo: VideoResourceType
+}
+
+const VideoRecommendCard:FC<IProps> = memo(({videoInfo}) => {
+    const formateView = useViewsFormater()
+
+    return (
+        <div className={styles.container}>
+            <Link to={`/video/${videoInfo.id}`} className={styles.link}>
+                <img className={styles.image} src={videoInfo.snippet.thumbnails.medium?.url} alt=""/>
+            </Link>
+            <div className={styles.infoContainer}>
+                <Link to={`/video/${videoInfo.id}`} className={styles.link}>
+                    <h3 className={styles.title}>{videoInfo.snippet.title}</h3>
+                </Link>
+                <Link to={`/channel/${videoInfo.snippet.channelId}`} className={styles.link}>
+                    <p className={styles.channelName}>{videoInfo.snippet.channelTitle}</p>
+                </Link>
+                <p className={styles.viewCount}><ViewIcon className={styles.viewCountIcon}/>{formateView(videoInfo.statistics.viewCount)}</p>
+            </div>
+        </div>
+    );
+});
+
+export default VideoRecommendCard;
