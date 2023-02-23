169fa320cb27ac3399396c0bec0d74a7132c2b74
Darkerros <Darkerros@yandex.ru>
2023-02-23 17:41:28 +0300

    feat: rework base layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import styles from './Layout.module.css'
 import {Outlet} from "react-router";
 import Header from "../Header/Header";
+import Menu from "../Menu/Menu";
 
 const Layout = () => {
     return (
         <>
             <Header/>
-            <Outlet/>
+            <div className={styles.container}>
+                <Menu/>
+                <main className={styles.contentContainer}>
+                    <Outlet/>
+                </main>
+            </div>
         </>
     );
 };
